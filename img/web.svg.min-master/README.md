ce7567237b12afced398f68516358ac3ac451bb7
Gjrv <gjrv-z@yandex.ru>
2017-04-22 22:27:50 +0400

    Site v_1.0

---
+++ img/web.svg.min-master/README.md
@@ -0,0 +1,23 @@
+# Web.svg.min
+Vector icons, badges, flags etc. über optimised for web use. (Formerly called social.svg.min).  
+
+<p>&nbsp;</p>
+
+Preview: [icons + badges](http://codepen.io/larsenwork/pen/admEZM), [flags](http://codepen.io/larsenwork/details/vLpyqV/)  
+Get them: [svg](https://github.com/larsenwork/social.svg.min/tree/master/svg) and [inline svg](https://github.com/larsenwork/web.svg.min/tree/master/inline%20svg)  
+Requests: [issue](https://github.com/larsenwork/social.svg.min/issues/2)
+
+<p>&nbsp;</p>
+
+<a href="http://codepen.io/larsenwork/pen/admEZM"><img src="http://i.imgur.com/lJweSdM.png" width="50%" /></a>
+
+<p>&nbsp;</p>
+
+#### Learn
+I wrote some short Medium articles: "Optimising SVGs for web use" [Part 1](https://medium.com/@larsenwork/optimising-svgs-for-web-use-part-1-67e8f2d4035), [Part 2](https://medium.com/@larsenwork/optimising-svgs-for-web-use-part-2-6711cc15df46) and [Part 2½](https://medium.com/@larsenwork/optimising-svgs-for-web-use-part-2-1-598815d74f9c)
+
+<p>&nbsp;</p>
+
+#### License
+Logos are © the respective brands/creators.  
+web.svg.min is free to use. It's provided "as is", without warranty.  
---
+++ js/bg_animate.js
@@ -0,0 +1,76 @@
+﻿debugger;
+var canvas = document.getElementById("bg-canvas"),
+    ctx = canvas.getContext('2d'),
+    triangles = [],
+    colors = ["#EFA580", "#9D5184", "#47183A", "#B42842","#2E1432"];
+
+var resize = function ()
+{
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientWidth;
+}
+window.addEventListener('resize', resize);
+resize();
+function Triangle()
+{
+    
+        this.coords = [{}, {}, {}];
+        this.pos = {};
+
+    this.init = function init()
+    {
+        this.pos.x = Math.random() * canvas.width;
+        this.pos.y = Math.random() * canvas.height;
+        this.coords[0].x =  Math.random() * 10;
+        this.coords[0].y =  20 + Math.random() * 80;
+        this.coords[1].x =  20 + Math.random() * 80;
+        this.coords[1].y =  Math.random() * 10;
+        this.coords[2].x =  50 + Math.random() * 100;
+        this.coords[2].y =  50 + Math.random() * 100;
+    }
+
+    this.draw = function ()
+    {
+        ctx.beginPath();
+        ctx.moveTo(this.coords[0].x + this.pos.x, this.coords[0].y + this.pos.y);
+        ctx.lineTo(this.coords[1].x + this.pos.x, this.coords[1].y + this.pos.y);
+        ctx.lineTo(this.coords[2].x + this.pos.x, this.coords[2].y + this.pos.y);
+        ctx.closePath();
+        ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+        ctx.fill();
+    };
+    this.animate = function ()
+    {
+        var i;
+        for (i = 0; i < config.length; i++) {
+
+            var star = config.stars[i];
+
+            if (star.y < 0 || star.y > canvas.height) {
+                star.vx = star.vx;
+                star.vy = - star.vy;
+            } else if (star.x < 0 || star.x > canvas.width) {
+                star.vx = - star.vx;
+                star.vy = star.vy;
+            }
+
+            star.x += star.vx;
+            star.y += star.vy;
+        }
+    };
+ 
+}
+
+function draw()
+{
+    
+    for (var i = 0; i < 15; i++)
+    {
+        var t = new Triangle();
+        t.init();
+        triangles.push(t);
+        triangles[i].draw();
+    }
+        
+}
+
